69887724001a269a874cf790b6943fb9fde86252
Cristi Cismas <cristicismas99@gmail.com>
2020-06-25 21:55:51 +0300

    Add a basic parser to parse a line of text in a vector of elements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+pub mod parser;
+
+use text_io::read;
+use parser::parse_line_to_elements;
+
+fn read_line() -> String {
+    let line: String = read!("{}\n");
+
+    return line;
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let line = read_line();
+        let elements = parse_line_to_elements(line);
+
+        for i in 0..elements.len() {
+            println!("{}", elements[i]);
+        }
+    }
 }
---
+++ src/parser.rs
@@ -0,0 +1,38 @@
+fn remove_whitespace(string: &str) -> String {
+  string.chars().filter(|character| !character.is_whitespace()).collect()
+}
+
+fn push_accumulator(vector: &mut Vec<String>, accumulator: &mut String) {
+  let copy_of_accumulator = accumulator.clone();
+  vector.push(copy_of_accumulator);
+
+  accumulator.clear();
+}
+
+pub fn parse_line_to_elements(line: String) -> Vec<String> {
+  let trimmed_line = remove_whitespace(&line);
+  
+  let symbols = ['+', '-', '/', '*', '%'];
+  let mut elements: Vec<String> = Vec::new();
+
+  let mut accumulator = String::new();
+
+  for index in 0..trimmed_line.chars().count() {
+    let current_char = trimmed_line.chars().nth(index).unwrap();
+
+    if symbols.contains(&current_char) {
+      push_accumulator(&mut elements, &mut accumulator);
+      accumulator.push(current_char);
+      push_accumulator(&mut elements, &mut accumulator);
+    } else {
+      accumulator.push(current_char);
+    }
+  }
+
+  let copy_of_accumulator = accumulator.clone();
+  elements.push(copy_of_accumulator);
+
+  accumulator.clear();
+
+  return elements;
+}
