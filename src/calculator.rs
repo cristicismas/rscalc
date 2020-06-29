c53f651353da948760dff224d17f10f280a253bb
Cristi Cismas <cristicismas99@gmail.com>
2020-06-29 13:09:25 +0300

    Add tests for all mathematical operations (that are currently implemented)

--- src/calculator.rs
+++ src/calculator.rs
@@ -29,9 +29,37 @@ mod tests {
   use super::*;
 
   #[test]
-  fn test_calculator() {
+  fn test_addition() {
     let elements = vec![String::from("4"), String::from("+"), String::from("5")];
 
     assert_eq!(calculate(elements), Ok(String::from("9")));
+  }
+
+  #[test]
+  fn test_subtraction() {
+    let elements = vec![String::from("10"), String::from("-"), String::from("2")];
+
+    assert_eq!(calculate(elements), Ok(String::from("8")));
+  }
+
+  #[test]
+  fn test_division() {
+    let elements = vec![String::from("10"), String::from("/"), String::from("5")];
+
+    assert_eq!(calculate(elements), Ok(String::from("2")));
+  }
+
+  #[test]
+  fn test_multiplication() {
+    let elements = vec![String::from("3"), String::from("*"), String::from("4")];
+
+    assert_eq!(calculate(elements), Ok(String::from("12")));
+  }
+
+  #[test]
+  fn test_modulo() {
+    let elements = vec![String::from("15"), String::from("%"), String::from("10")];
+
+    assert_eq!(calculate(elements), Ok(String::from("5")));
   }
 }
